f1e59cf47a0e8cae4e3e3a115f5833914fd737b8
OG - LOC <82328185+Vapor1998@users.noreply.github.com>
2025-06-30 00:57:08 +0530

    Add files via upload

---
+++ Retreving and Filtering Data Sql/Retrieveing Data Assignment.sql
@@ -0,0 +1,42 @@
+
+
+CREATE TABLE movies(
+MovieNo int,
+MovieTitle varchar(50),
+Rating float,
+Dislikes int,
+Likes int, 
+ReleaseMonth varchar(15)
+);
+
+INSERT INTO movies VALUES
+(
+100,'SherShah',4.5,15,88,'May'
+);
+INSERT INTO movies VALUES
+(101,'Hungama',3.2,52,51,'June');
+
+INSERT INTO movies VALUES
+(102,'War',3.6,38,86,'August');
+
+INSERT INTO movies VALUES
+(103,'Bhuj',4.2,22,90,'January');
+
+INSERT INTO movies VALUES
+(104,'Roohi',3.8,28,74,'May');
+
+INSERT INTO movies VALUES
+(105,'Mimi',4.2,28,76,'June');
+
+INSERT INTO movies VALUES
+(106,'Pagglait',4,29,82,'June');
+
+INSERT INTO movies VALUES
+(107,'Saina',3.9,27,92,'October');
+
+INSERT INTO movies VALUES
+(108,'Drive',3,56,42,'January');
+
+INSERT INTO movies VALUES
+(109,'Guilty',4.2,12,86,'August');
+
